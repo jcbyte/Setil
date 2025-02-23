c3b10ec98267d5eae75b69de57ce94b03688c7a6
Joel Cutler <joelcutler108@gmail.com>
2025-02-23 00:52:44 +0000

    create base of group page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
-import { ref } from "vue";
-
-const count = ref(0);
+import GroupPage from "./components/GroupPage.vue";
 </script>
 
 <template>
 	<div class="flex justify-center items-center">
-		<div class="w-fit flex flex-col justify-center items-center gap-2">
-			<p>Hello Vue</p>
-			<button class="bg-zinc-300 p-2 rounded-lg text-zinc-900" type="button" @click="count++">
-				count is {{ count }}
-			</button>
-		</div>
+		<GroupPage
+			:group="{
+				name: 'group',
+				owner: 'owner',
+				users: [{ id: 'owner', name: 'OWNER' }],
+				transactions: [
+					{ title: 'title', amount: 0, from: ['aa', 'bb'], to: ['cc'], date: new Date() },
+					{ title: 'title2', amount: 1000, from: ['aa'], to: ['bb', 'cc'], date: new Date() },
+				],
+			}"
+		/>
 	</div>
 </template>
---
+++ src/components/GroupPage.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { Group } from "../types";
+
+defineProps<{
+	group: Group;
+}>();
+
+function joinList(l: string[]): string {
+	return l.join(", ");
+}
+</script>
+
+<template>
+	<div class="flex flex-col gap-2">
+		<div class="text-xl">{{ group.name }}</div>
+		<div class="bg-zinc-700 w-80 rounded-lg p-2 flex flex-col" v-for="transition in group.transactions">
+			<div class="flex justify-between">
+				<div class="text-lg">{{ transition.title }}</div>
+				<div class="text-lg">{{ transition.amount }}</div>
+			</div>
+			<div class="text-sm text-zinc-300">{{ transition.date.toLocaleString() }}</div>
+			<div class="text-sm">{{ `${joinList(transition.from)}->${joinList(transition.to)}` }}</div>
+		</div>
+	</div>
+</template>
