4aa03171e9697d59ed0b5d1aaccc786af405a664
Joel Cutler <joelcutler108@gmail.com>
2025-02-23 16:54:37 +0000

    updated sign in page

--- src/pages/signInPage.vue
+++ src/pages/signInPage.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import { Button } from "primevue";
 import { signInWithGoogle } from "../firebase/auth";
 </script>
 
 <template>
-	<div>
-		<div class="p-4 bg-red-500" @click="signInWithGoogle()">Google Sign In</div>
+	<div class="flex flex-col items-center justify-center p-6 gap-4">
+		<h1 class="text-2xl font-bold">You Must Sign In to Use This App</h1>
+		<Button @click="signInWithGoogle()">
+			<div class="flex items-center justify-center gap-2">
+				<i class="pi pi-google" />
+				<span class="font-semibold">Sign In with Google</span>
+			</div>
+		</Button>
 	</div>
 </template>
