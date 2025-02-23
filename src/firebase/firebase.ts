66ecedf975346b41a8ce3121e31fe87e88205d84
Joel Cutler <joelcutler108@gmail.com>
2025-02-23 16:07:53 +0000

    added firebase

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyDdxl38UXQp0XrXx9ZENB1BjIR2JC2nMr4",
+	authDomain: "setil-420xd.firebaseapp.com",
+	projectId: "setil-420xd",
+	storageBucket: "setil-420xd.firebasestorage.app",
+	messagingSenderId: "913646123341",
+	appId: "1:913646123341:web:83bdc33b50b509f5dc5f04",
+	measurementId: "G-61RDYF85Q1",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
