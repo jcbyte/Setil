6239bd8392dda70a8469542d9002177c3767ab2a
Joel Cutler <joelcutler108@gmail.com>
2025-02-23 16:54:27 +0000

    added prime vue components

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import Material from "@primeuix/themes/material";
+import "primeicons/primeicons.css";
+import PrimeVue from "primevue/config";
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./style.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue, {
+	theme: {
+		preset: Material,
+	},
+});
+app.mount("#app");
