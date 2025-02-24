e1aa82b63646c144cbb4f4529c4814ab255d71b6
Joel Cutler <joelcutler108@gmail.com>
2025-02-24 23:55:16 +0000

    get group data

--- src/pages/GroupPage.vue
+++ src/pages/GroupPage.vue
@@ -1,17 +1,43 @@
 <script setup lang="ts">
+import { useToast } from "primevue";
+import { onMounted, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
 import { usePageTitle } from "../composables/usePageTitle";
+import { getGroupData, type GroupData } from "../firebase/firestore";
 
-usePageTitle("$$$");
+const group = ref<GroupData | null>(null);
+const route = useRoute();
+const router = useRouter();
+const toast = useToast();
+const setPageTitle = usePageTitle("Group");
 
-const group = {
-	name: "group",
-	owner: "owner",
-	users: [{ id: "owner", name: "OWNER" }],
-	transactions: [
-		{ title: "title", amount: 0, from: ["aa", "bb"], to: ["cc"], date: new Date() },
-		{ title: "title2", amount: 1000, from: ["aa"], to: ["bb", "cc"], date: new Date() },
-	],
-};
+onMounted(() => {
+	function returnHome() {
+		toast.add({
+			severity: "error",
+			summary: "Invalid Group",
+			life: 2000,
+		});
+
+		router.push("/");
+	}
+
+	const groupId = !Array.isArray(route.params.groupId) ? route.params.groupId : null;
+	if (!groupId) {
+		returnHome();
+		return;
+	}
+
+	getGroupData(groupId).then((data) => {
+		if (!data) {
+			returnHome();
+			return;
+		}
+
+		group.value = data;
+		setPageTitle(data.name);
+	});
+});
 
 function joinList(l: string[]): string {
 	return l.join(", ");
@@ -22,14 +48,13 @@ function joinList(l: string[]): string {
 
 <template>
 	<div class="flex flex-col gap-2">
-		<div class="text-xl">{{ group.name }}</div>
-		<div class="bg-zinc-700 w-80 rounded-lg p-2 flex flex-col" v-for="transition in group.transactions">
+		<!-- <div class="bg-zinc-700 w-80 rounded-lg p-2 flex flex-col" v-for="transition in group.transactions">
 			<div class="flex justify-between">
 				<div class="text-lg">{{ transition.title }}</div>
 				<div class="text-lg">{{ transition.amount }}</div>
 			</div>
 			<div class="text-sm text-zinc-300">{{ transition.date.toLocaleString() }}</div>
 			<div class="text-sm">{{ `${joinList(transition.from)}->${joinList(transition.to)}` }}</div>
-		</div>
+		</div> -->
 	</div>
 </template>
