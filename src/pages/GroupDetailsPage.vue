de3c9c313d204171eaa76244be21e13d984488c0
Joel Cutler <joelcutler108@gmail.com>
2025-03-16 23:22:45 +0000

    added group details form

--- src/pages/GroupDetailsPage.vue
+++ src/pages/GroupDetailsPage.vue
@@ -1,194 +1,107 @@
 <script setup lang="ts">
-import { Form, type FormResolverOptions, type FormSubmitEvent } from "@primevue/forms";
-import { Button, FloatLabel, InputText, Message, Select, useToast } from "primevue";
-import { computed, reactive, ref } from "vue";
+import { Button } from "@/components/ui/button";
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import YourAccountSettings from "@/components/YourAccountSettings.vue";
+import { CurrencySettings } from "@/util/groupSettings";
 import { useRoute, useRouter } from "vue-router";
 import { useGroup } from "../composables/useGroup";
-import { usePageTitle } from "../composables/usePageTitle";
-import {
-	createGroup,
-	deleteGroup as firestoreDeleteGroup,
-	leaveGroup as firestoreLeaveGroup,
-	getUser,
-	updateGroup,
-} from "../firebase/firestore";
-import type { GroupData } from "../firebase/types";
-import { CurrencySettings, type Currency } from "../util/groupSettings";
 
-const toast = useToast();
+import { toTypedSchema } from "@vee-validate/zod";
+import { useForm } from "vee-validate";
+import * as z from "zod";
+
 const router = useRouter();
-
 const route = useRoute();
 const routeGroupId = Array.isArray(route.params.groupId) ? route.params.groupId[0] : route.params.groupId || null;
 
-usePageTitle({ title: routeGroupId ? "Edit Group" : "Create Group" });
+const { groupId, groupData } = useGroup(routeGroupId, () => {
+	// load init values
+});
 
-const creatingGroup = ref<boolean>(false);
-
-interface CurrencyOption {
-	id: Currency;
-	name: string;
-}
-const currencyOptions = computed<CurrencyOption[]>(() =>
-	Object.entries(CurrencySettings).map(([currency, currencySetting]) => ({
-		id: currency as Currency,
-		name: currencySetting.name,
-	}))
+const formSchema = toTypedSchema(
+	z.object({
+		name: z.string(),
+		description: z.string(),
+		currency: z.string(),
+	})
 );
 
-const initialValues = reactive<{ name: string | null; currency: CurrencyOption | null }>({
-	name: null,
-	currency: null,
+const { isFieldDirty, handleSubmit } = useForm({
+	validationSchema: formSchema,
 });
 
-const { groupData } = useGroup(routeGroupId, () => {
-	if (routeGroupId) {
-		initialValues.name = groupData.value!.name;
-		initialValues.currency =
-			currencyOptions.value.find((currencyOption) => currencyOption.id === groupData.value!.currency) ?? null;
-	}
+const onSubmit = handleSubmit((values) => {
+	console.log(values);
 });
-
-const formResolver = ({ values }: FormResolverOptions): Record<string, any> => {
-	const errors: Record<string, any> = {};
-
-	if (!values.name) {
-		errors.name = ["Name is required."];
-	}
-
-	if (!values.currency) {
-		errors.currency = ["Currency is required."];
-	}
-
-	return {
-		values,
-		errors,
-	};
-};
-
-async function formSubmit({ valid, values }: FormSubmitEvent): Promise<void> {
-	if (valid) {
-		creatingGroup.value = true;
-
-		const groupData: Omit<GroupData, "owner"> = {
-			name: values.name,
-			currency: values.currency.id,
-		};
-
-		let updatedGroupId = routeGroupId;
-		if (routeGroupId) {
-			await updateGroup(routeGroupId, groupData);
-
-			toast.add({
-				severity: "success",
-				summary: "Group updated",
-				life: 2000,
-			});
-		} else {
-			updatedGroupId = await createGroup(groupData);
-
-			toast.add({
-				severity: "success",
-				summary: "Group created",
-				life: 2000,
-			});
-		}
-
-		router.push(`/group/${updatedGroupId}`);
-		creatingGroup.value = false;
-	}
-}
-
-async function leaveGroup(): Promise<void> {
-	creatingGroup.value = true;
-
-	await firestoreLeaveGroup(routeGroupId!);
-
-	toast.add({
-		severity: "success",
-		summary: "Left group",
-		life: 2000,
-	});
-
-	router.push(`/`);
-
-	creatingGroup.value = false;
-}
-
-async function deleteGroup(): Promise<void> {
-	creatingGroup.value = true;
-
-	await firestoreDeleteGroup(routeGroupId!);
-
-	toast.add({
-		severity: "success",
-		summary: "Group deleted",
-		life: 2000,
-	});
-
-	router.push(`/`);
-
-	creatingGroup.value = false;
-}
 </script>
 
 <template>
-	<Form
-		v-slot="$form"
-		:initialValues
-		:resolver="formResolver"
-		@submit="formSubmit"
-		class="w-80 flex flex-col items-center gap-2"
-	>
-		<div class="flex flex-col gap-1 w-full">
-			<FloatLabel variant="on">
-				<InputText id="name_text" name="name" autocomplete="off" fluid />
-				<label for="name_text">Group Name</label>
-			</FloatLabel>
-			<Message v-if="$form.name?.invalid" severity="error" size="small" variant="simple">
-				{{ $form.name.error }}
-			</Message>
+	<div class="w-full flex flex-col gap-4 items-center">
+		<div class="w-full flex justify-between items-center">
+			<div class="flex gap-2 justify-center items-center">
+				<Button variant="ghost" class="size-9" @click="router.push(`/group/${routeGroupId}`)">
+					<i class="pi pi-arrow-left" />
+				</Button>
+				<span v-if="groupId" class="text-lg font-semibold">Group Settings</span>
+				<Skeleton v-else class="w-20 h-7" />
+			</div>
+			<YourAccountSettings />
 		</div>
 
-		<div class="flex flex-col gap-1 w-full">
-			<FloatLabel variant="on">
-				<Select id="currency_select" name="currency" :options="currencyOptions" optionLabel="name" fluid />
-				<label for="currency_select">Currency</label>
-			</FloatLabel>
-			<Message v-if="$form.currency?.invalid" severity="error" size="small" variant="simple">
-				{{ $form.currency.error }}
-			</Message>
+		<div class="border border-zinc-800 rounded-lg flex flex-col gap-4 p-4">
+			<div class="flex flex-col">
+				<span class="text-lg font-semibold">Group Details</span>
+				<span class="text-sm text-zinc-400">Update your group information</span>
+			</div>
+
+			<form @submit="onSubmit">
+				<FormField v-slot="{ componentField }" name="name" :validate-on-blur="!isFieldDirty">
+					<FormItem v-auto-animate>
+						<FormLabel>Group Name</FormLabel>
+						<FormControl>
+							<Input type="text" placeholder="Germany Trip" v-bind="componentField" />
+						</FormControl>
+						<FormMessage />
+					</FormItem>
+				</FormField>
+
+				<FormField v-slot="{ componentField }" name="description" :validate-on-blur="!isFieldDirty">
+					<FormItem v-auto-animate>
+						<FormLabel>Description</FormLabel>
+						<FormControl>
+							<Textarea placeholder="Expenses for Munich Trip." v-bind="componentField" />
+						</FormControl>
+						<FormMessage />
+					</FormItem>
+				</FormField>
+
+				<FormField v-slot="{ componentField }" name="currency" :validate-on-blur="!isFieldDirty">
+					<FormItem v-auto-animate>
+						<FormLabel>Currency</FormLabel>
+						<FormControl>
+							<Select v-bind="componentField">
+								<SelectTrigger>
+									<SelectValue placeholder="Euro (€)" />
+								</SelectTrigger>
+								<SelectContent>
+									<SelectItem v-for="(currency, currencyId) in CurrencySettings" :value="currencyId">
+										{{ currency.name }} ({{ currency.symbol }})
+									</SelectItem>
+								</SelectContent>
+							</Select>
+						</FormControl>
+						<FormMessage />
+					</FormItem>
+				</FormField>
+
+				<Button type="submit">
+					<i class="pi pi-save" />
+					<span>Save</span>
+				</Button>
+			</form>
 		</div>
-
-		<Button
-			type="submit"
-			:icon="routeGroupId ? 'pi pi-pencil' : 'pi pi-plus'"
-			:label="routeGroupId ? 'Update' : 'Create'"
-			:loading="creatingGroup"
-			severity="secondary"
-			fluid
-		/>
-
-		<Button
-			v-if="routeGroupId"
-			type="button"
-			icon="pi pi-sign-out"
-			label="Leave"
-			:loading="creatingGroup"
-			severity="danger"
-			fluid
-			@click="leaveGroup"
-		/>
-
-		<Button
-			v-if="routeGroupId && getUser().uid === groupData?.owner"
-			type="button"
-			icon="pi pi-trash"
-			label="Delete"
-			:loading="creatingGroup"
-			severity="danger"
-			fluid
-			@click="deleteGroup"
-		/>
-	</Form>
+	</div>
 </template>
