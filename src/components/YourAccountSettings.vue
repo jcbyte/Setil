d81ee3adba83e07fa78fd76500f8a7c7ded022e8
Joel Cutler <joelcutler108@gmail.com>
2025-03-16 15:54:57 +0000

    converted your account settings into a custom component

---
+++ src/components/YourAccountSettings.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import Avatar from "@/components/Avatar.vue";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { firebaseSignOut } from "@/firebase/auth";
+import { getUser } from "@/firebase/firestore";
+import { useToast } from "primevue";
+
+const toast = useToast();
+
+function signOut() {
+	firebaseSignOut()
+		.then(() => {
+			toast.add({
+				severity: "success",
+				summary: "Signed Out",
+				life: 2000,
+			});
+		})
+		.catch((error) => {
+			toast.add({
+				severity: "error",
+				summary: "Sign Out Failed",
+				detail: error.message,
+				life: 5000,
+			});
+		});
+}
+
+const user = getUser();
+</script>
+
+<template>
+	<DropdownMenu>
+		<DropdownMenuTrigger as-child>
+			<Avatar class="size-9" :src="user.photoURL" :name="user.displayName ?? 'Myself'" />
+		</DropdownMenuTrigger>
+		<DropdownMenuContent>
+			<DropdownMenuItem @click="signOut">
+				<div class="w-full flex justify-between">
+					<span class="text-red-400">Sign Out</span>
+					<i class="pi pi-sign-out text-red-400" />
+				</div>
+			</DropdownMenuItem>
+		</DropdownMenuContent>
+	</DropdownMenu>
+</template>
--- src/pages/GroupListPage.vue
+++ src/pages/GroupListPage.vue
@@ -1,52 +1,21 @@
 <script setup lang="ts">
-import Avatar from "@/components/Avatar.vue";
 import GroupListItem from "@/components/GroupListItem.vue";
 import { Button } from "@/components/ui/button";
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { Skeleton } from "@/components/ui/skeleton";
-import { firebaseSignOut } from "@/firebase/auth";
-import { getUser, getUserGroups, type ExtendedGroupData } from "@/firebase/firestore";
-import { useToast } from "primevue";
-import { computed, onMounted, ref } from "vue";
+import YourAccountSettings from "@/components/YourAccountSettings.vue";
+import { getUserGroups, type ExtendedGroupData } from "@/firebase/firestore";
+import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
 
 const groups = ref<Record<string, ExtendedGroupData> | null>(null);
 
-const user = computed(() => getUser());
-
-const toast = useToast();
-
 onMounted(() => {
 	getUserGroups().then((groupsList) => {
 		groups.value = groupsList;
 	});
 });
-
-function signOut() {
-	firebaseSignOut()
-		.then(() => {
-			toast.add({
-				severity: "success",
-				summary: "Signed Out",
-				life: 2000,
-			});
-		})
-		.catch((error) => {
-			toast.add({
-				severity: "error",
-				summary: "Sign Out Failed",
-				detail: error.message,
-				life: 5000,
-			});
-		});
-}
 </script>
 
 <template>
@@ -54,19 +23,7 @@ function signOut() {
 		<div class="flex justify-between">
 			<span class="text-lg font-semibold">My Groups</span>
 			<div class="flex gap-2 justify-center items-center">
-				<DropdownMenu>
-					<DropdownMenuTrigger as-child>
-						<Avatar class="size-9" :src="user.photoURL" :name="user.displayName ?? 'Myself'" />
-					</DropdownMenuTrigger>
-					<DropdownMenuContent>
-						<DropdownMenuItem @click="signOut">
-							<div class="w-full flex justify-between">
-								<span class="text-red-400">Sign Out</span>
-								<i class="pi pi-sign-out text-red-400" />
-							</div>
-						</DropdownMenuItem>
-					</DropdownMenuContent>
-				</DropdownMenu>
+				<YourAccountSettings />
 				<Button @click="router.push('/create')">
 					<i class="pi pi-plus-circle font-semibold" />
 					<span class="font-semibold">New Group</span>
