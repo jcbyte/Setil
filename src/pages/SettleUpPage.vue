ec75059544ecf755279b455b6b0283f26b794f66
Joel Cutler <joelcutler108@gmail.com>
2025-03-17 16:48:45 +0000

    removed old settle up page

--- src/pages/SettleUpPage.vue
+++ src/pages/SettleUpPage.vue
@@ -1,34 +1,3 @@
-<script setup lang="ts">
-import { computed } from "vue";
-import { useRoute } from "vue-router";
-import { useGroup } from "../composables/useGroup";
-import type { GroupUserData } from "../firebase/types";
-import { formatCurrency } from "../util/util";
+<script setup lang="ts"></script>
 
-const route = useRoute();
-
-const routeGroupId = Array.isArray(route.params.groupId) ? route.params.groupId[0] : route.params.groupId || null;
-const { groupData, users } = useGroup(routeGroupId);
-
-const usersWithTransactions = computed<Record<string, GroupUserData>>(() => {
-	return Object.fromEntries(
-		Object.entries(users.value!).filter(([_userId, userData]) =>
-			Object.values(userData.balance).some((balance) => balance < 0)
-		)
-	);
-});
-</script>
-
-<template>
-	<div class="flex flex-col gap-2">
-		<div class="bg-zinc-700 w-80 rounded-lg p-2 flex flex-col" v-for="(user, _userId) in usersWithTransactions">
-			<div class="text-lg">{{ user.name }}</div>
-			<div class="text-md" v-for="(otherUserBalance, otherUserId) in user.balance">
-				<div v-if="otherUserBalance < 0">
-					Send {{ formatCurrency(-otherUserBalance / 100, groupData!.currency) }} to
-					{{ users![otherUserId].name }}
-				</div>
-			</div>
-		</div>
-	</div>
-</template>
+<template>todo</template>
