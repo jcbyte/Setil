011b3aa2488407599dad90977649b3dcc06cfdb5
Joel Cutler <joelcutler108@gmail.com>
2025-03-20 21:42:47 +0000

    created 404 page

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+</script>
+
+<template>
+	<div class="flex flex-col items-center justify-center gap-4 border border-border p-8 rounded-lg min-w-80">
+		<div class="flex flex-col items-center justify-center">
+			<span class="text-2xl font-bold">Error 404</span>
+			<span class="text-muted-foreground">This Page cannot be found</span>
+		</div>
+		<Separator />
+		<Button @click="router.push('/')" class="w-full">
+			<span class="font-semibold">Return Home</span>
+		</Button>
+	</div>
+</template>
