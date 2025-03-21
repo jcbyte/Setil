017d440ff20ed1aa4cff24bb69bfe664efe1bf06
Joel Cutler <joelcutler108@gmail.com>
2025-03-21 22:08:57 +0000

    added animation to firebase loader

--- src/App.vue
+++ src/App.vue
@@ -20,17 +20,34 @@ useColorMode().value = "dark";
 </script>
 
 <template>
-	<div v-if="firebaseLoaded" class="flex justify-center items-center p-4">
-		<SignInPage v-if="!currentUser" />
-		<router-view v-else />
-	</div>
-	<div v-else class="flex flex-col justify-center items-center gap-4 w-full p-4 pt-12">
-		<img src="/icon/icon-192.png" alt="App Logo" class="size-24" />
-		<div class="flex gap-2 items-center">
-			<LoaderCircle :stroke-width="3" class="animate-spin text-muted-foreground" />
-			<p class="text-lg font-bold text-muted-foreground">Initialising Setil</p>
+	<Transition name="loader">
+		<div v-if="firebaseLoaded" class="flex justify-center items-center p-4">
+			<SignInPage v-if="!currentUser" />
+			<router-view v-else />
 		</div>
-	</div>
+		<div v-else class="fixed top-12 flex flex-col justify-center items-center gap-4 w-full p-4">
+			<img src="/icon/icon-192.png" alt="App Logo" class="size-24" />
+			<div class="flex gap-2 items-center">
+				<LoaderCircle :stroke-width="3" class="animate-spin text-muted-foreground" />
+				<p class="text-lg font-bold text-muted-foreground">Initialising Setil</p>
+			</div>
+		</div>
+	</Transition>
 
 	<Toaster />
 </template>
+
+<style scoped>
+.loader-enter-active,
+.loader-leave-active {
+	transition: 0.2s ease;
+}
+
+.loader-enter-from {
+	opacity: 0;
+}
+
+.loader-leave-to {
+	transform: translateY(calc(-100% - 3rem));
+}
+</style>
