35909906a0f1887d88734a97cd70dec6d31a7861
Joel Cutler <joelcutler108@gmail.com>
2025-03-27 16:33:17 +0000

    include firebase initialisation in sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,21 +1,21 @@
-// importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
-// importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-// 	apiKey: "AIzaSyDdxl38UXQp0XrXx9ZENB1BjIR2JC2nMr4",
-// 	authDomain: "setil-420xd.firebaseapp.com",
-// 	projectId: "setil-420xd",
-// 	storageBucket: "setil-420xd.firebasestorage.app",
-// 	messagingSenderId: "913646123341",
-// 	appId: "1:913646123341:web:83bdc33b50b509f5dc5f04",
-// 	measurementId: "G-61RDYF85Q1",
-// };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyDdxl38UXQp0XrXx9ZENB1BjIR2JC2nMr4",
+	authDomain: "setil-420xd.firebaseapp.com",
+	projectId: "setil-420xd",
+	storageBucket: "setil-420xd.firebasestorage.app",
+	messagingSenderId: "913646123341",
+	appId: "1:913646123341:web:83bdc33b50b509f5dc5f04",
+	measurementId: "G-61RDYF85Q1",
+};
 
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-// const messaging = firebase.messaging();
+const messaging = firebase.messaging();
 
 // Handle messages when in the background
 // messaging.onBackgroundMessage(function (payload) {
@@ -28,27 +28,27 @@
 // 	self.registration.showNotification(notificationTitle, notificationOptions);
 // });
 
-// self.addEventListener("notificationclick", function (event) {
-// 	// Close the notification
-// 	event.notification.close();
+self.addEventListener("notificationclick", function (event) {
+	// Close the notification
+	event.notification.close();
 
-// 	// Extract route to open too
-// 	const appLink = "https://setil.vercel.app";
-// 	const route = event.notification.data?.route;
-// 	console.log(route);
-// 	// todo load route
+	const appLink = "https://setil.vercel.app";
+	// Extract route to open too
+	const route = event.notification.data?.route;
+	console.log(route);
+	// todo load route
 
-// 	event.waitUntil(
-// 		clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
-// 			// Focus on the open tab if it exists
-// 			for (let client of clientList) {
-// 				if (client.url === appLink && "focus" in client) {
-// 					return client.focus();
-// 				}
-// 			}
+	event.waitUntil(
+		clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
+			// Focus on the open tab if it exists
+			for (let client of clientList) {
+				if (client.url === appLink && "focus" in client) {
+					return client.focus();
+				}
+			}
 
-// 			// Open a new tab if no matching window is open
-// 			return clients.openWindow(appLink);
-// 		})
-// 	);
-// });
+			// Open a new tab if no matching window is open
+			return clients.openWindow(appLink);
+		})
+	);
+});
